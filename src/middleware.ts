9d48b3d452a177410a8655f7e8d7c3e4a97de3dd
Buyeon Hwang <arpa1705@gmail.com>
2024-09-21 01:43:00 +0900

    feat: NextAuth 미들웨어 추가

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { auth } from '@/auth';
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== '/login') {
+    const newUrl = new URL('/login', req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
