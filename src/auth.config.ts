964b9e454aae28d4dfaf17015244133812c43f8a
알파 <arpa1705@gmail.com>
2024-09-20 18:56:02 +0900

    feat: 카카오 Auth 제거

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,10 +1,9 @@
 import type { NextAuthConfig } from 'next-auth';
 
 import Google from 'next-auth/providers/google';
-import Kakao from 'next-auth/providers/kakao';
 
 export default {
-  providers: [Google, Kakao],
+  providers: [Google],
   secret: process.env.AUTH_SECRET,
   pages: {
     signIn: '/login',
