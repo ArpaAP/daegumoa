e3c3175bbf78a877c2acd6007690e7e679504136
jamie2779 <jamie2779@naver.com>
2024-09-22 08:11:45 +0900

    feat: mission관련 db 연동

--- src/app/mission/page.client.tsx
+++ src/app/mission/page.client.tsx
@@ -6,8 +6,15 @@ import { eventTestData } from '@/constants/eventtest';
 import { missionTestData } from '@/constants/missiontest';
 
 import { Box, Text, VStack, Tabs, Tab, TabList, TabPanel, TabPanels } from '@chakra-ui/react';
+import { Mission, Event, MissionHolder } from '@prisma/client';
 
-export default function MissionPageContent() {
+type MissionWithEventAndHolder = Mission & { event: Event } & { missionHolders: MissionHolder[] };
+
+interface MissionListProps {
+  missions: MissionWithEventAndHolder[];
+}
+
+export default function MissionPageContent({ missions }: MissionListProps) {
   return (
     <Box h="100dvh" px={6} py={12}>
       <Box px={3} pb={6}>
@@ -19,7 +26,7 @@ export default function MissionPageContent() {
         </Text>
       </Box>
 
-      <Tabs variant="outlined" size="sm">
+      <Tabs variant="outlined" size="sm" pb="100px">
         <TabList mb={4} px={3}>
           <Tab>전체</Tab>
           <Tab>시장</Tab>
@@ -30,17 +37,37 @@ export default function MissionPageContent() {
         <TabPanels>
           <TabPanel p={0}>
             <VStack gap="20px">
-              <MissionCard mission={{ ...missionTestData, event: eventTestData, holders: [] }} />
+              {missions.map((mission, index) => (
+                <MissionCard key={index} mission={mission} />
+              ))}
             </VStack>
           </TabPanel>
-          <TabPanel>
-            <p>two!</p>
+          <TabPanel p={0}>
+            <VStack gap="20px">
+              {missions
+                .filter((mission) => mission.tag == 'MARKET')
+                .map((mission, index) => (
+                  <MissionCard key={index} mission={mission} />
+                ))}
+            </VStack>
           </TabPanel>
-          <TabPanel>
-            <p>three!</p>
+          <TabPanel p={0}>
+            <VStack gap="20px">
+              {missions
+                .filter((mission) => mission.tag == 'FESTIVAL')
+                .map((mission, index) => (
+                  <MissionCard key={index} mission={mission} />
+                ))}
+            </VStack>
           </TabPanel>
-          <TabPanel>
-            <p>four!</p>
+          <TabPanel p={0}>
+            <VStack gap="20px">
+              {missions
+                .filter((mission) => mission.tag == 'ETC')
+                .map((mission, index) => (
+                  <MissionCard key={index} mission={mission} />
+                ))}
+            </VStack>
           </TabPanel>
         </TabPanels>
       </Tabs>
