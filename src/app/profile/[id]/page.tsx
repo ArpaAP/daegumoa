bd2402ea55ca8fc0a334c0fe1da1bb333727c44d
jamie2779 <jamie2779@naver.com>
2024-09-22 14:23:34 +0900

    feat: /profile로 접근 시 본인 프로필로 리다이렉트

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,11 @@
 import ProfileContent from './page.client';
+import { auth } from '@/auth';
 import prisma from '@/lib/prisma';
 
 export default async function Profile({ params }: { params: { id: string } }) {
   const userId = params.id;
+  const session = await auth();
+
   const user = await prisma.user.findUnique({
     where: { id: parseInt(userId) },
     include: {
@@ -20,5 +23,14 @@ export default async function Profile({ params }: { params: { id: string } }) {
       missionHolders: true,
     },
   });
-  return <ProfileContent user={user} />;
+
+  if (session?.user?.email) {
+    const viewer = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    });
+
+    return <ProfileContent user={user} viewerId={viewer?.id ?? null} />;
+  } else {
+    return <></>;
+  }
 }
